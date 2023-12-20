9e484a077955a0757a2d151728c6abaec8231d07
bowlarbear <bowlarbear@proton.me>
2023-12-20 14:39:25 -0500

    added better error message to collect_shards()

--- src/main.rs
+++ src/main.rs
@@ -573,7 +573,7 @@ async fn collect_shards() -> Result<String, String> {
 	//obtain a list of all of the filenames in $HOME/shards
 	let shards = Command::new(&("ls")).args([&(get_home().unwrap()+"/shards")]).output().unwrap();
 	if !shards.status.success() {
-	return Err(format!("ERROR in collect_shards() with parsing $HOME/shards"));
+	return Err(format!("ERROR in collect_shards() with parsing $HOME/shards = {}", std::str::from_utf8(&shards.stderr).unwrap()));
 	} 
 	//convert the list of shards into a vector of results
 	let shards_output = std::str::from_utf8(&shards.stdout).unwrap();
@@ -583,7 +583,7 @@ async fn collect_shards() -> Result<String, String> {
 	for i in shards_vec{
 		let output = Command::new("cp").args([&(get_home().unwrap()+"/shards"+&(i.to_string())), "/mnt/ramdisk/CDROM/shards"]).output().unwrap();
 		if !output.status.success() {
-			return Err(format!("Error in collect_shards() with copying shards"))
+			return Err(format!("Error in collect_shards() with copying shard {} = {}", i.to_string(), std::str::from_utf8(&output.stderr).unwrap()))
 		}
 		} 
 	Ok(format!("SUCCESS in collecting shards"))
